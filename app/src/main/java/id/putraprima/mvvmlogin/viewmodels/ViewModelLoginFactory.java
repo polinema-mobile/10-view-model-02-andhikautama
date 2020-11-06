13f8f6979ad76c0437bd1fbb7fb498e0527823bc
Andhika Utama <andhikautamaa04@gmail.com>
2020-11-06 17:14:05 +0700

    Tugas View Model 02

---
+++ app/src/main/java/id/putraprima/mvvmlogin/viewmodels/ViewModelLoginFactory.java
@@ -0,0 +1,26 @@
+package id.putraprima.mvvmlogin.viewmodels;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+
+
+import id.putraprima.mvvmlogin.models.ModelLogin;
+
+public class ViewModelLoginFactory implements ViewModelProvider.Factory {
+
+    private ModelLogin modelLogin;
+
+    public ViewModelLoginFactory(ModelLogin modelLogin){
+        this.modelLogin = modelLogin;
+    }
+
+    @NonNull
+    @Override
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+        if(modelClass.isAssignableFrom(ViewModelLogin.class)){
+            return (T) new ViewModelLogin(modelLogin);
+        }
+        throw new IllegalArgumentException("Permintaan ViewModel ViewModelLogin");
+    }
+}
